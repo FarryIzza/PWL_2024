cba06b75b1f08be0f61999bdf58e012107997cd5
FarryIzza <2131710106@student.polinema.ac.id>
2024-05-29 17:21:41 +0700

    pertemuan 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello', function () {
     return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'parry';
+    });
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/about', function () {
+    return '2141762106 M Farry Izza F';
+    });        
+Route::get('/', function () {
+   return view ('welcome');
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+Route::get('/user1/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user2/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
+    Route::get('/hello', [WelcomeController::class,'hello']);
+
+    use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+    ]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+    ]);
